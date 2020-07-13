1d738c1b9d299ce79c45b100071e7e2109781a67
fkubota <rkkubotay@gmail.com>
2020-07-13 13:36:34 +0900

    [Update] pandasを追加

--- src/df_handler.py
+++ src/df_handler.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+
+class DfHandler:
+    def __init__(self):
+        self.df_seg = pd.DataFrame()
+
+
+def main():
+    print('hoge')
+
+
+if __name__ == '__main__':
+    main()
